a22a8bec991a98fab2e0653e922ba0d46108b519
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-24 13:02:44 +0300

    Copy the SQL of queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN 2016-01-01 AND 2019-12-31;
+SELECT name FROM animals WHERE escape_attempts <3 and neutered = true;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.50;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
