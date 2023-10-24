f505d52ddd8411a02072473aa34e64e0f15e4c42
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-24 18:18:19 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,65 @@ SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.50;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
+
+BEGIN;
+UPDATE animals
+SET species = NULL;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon%';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE birthdate > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+SELECT * FROM animals;
+ROLLBACK TO SP1;
+SELECT * FROM animals;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+SELECT COUNT(*) FROM animals;
+SELECT 
+   COUNT(escape_attempts) 
+FROM 
+   animals
+WHERE escape_attempts = '0';
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(*)
+FROM animals
+WHERE escape_attempts > 0
+GROUP BY neutered;
+SELECT neutered,
+       MIN(weight_kg),
+       MAX(weight_kg)
+FROM animals
+GROUP BY neutered;
+SELECT neutered,
+       AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31'
+GROUP BY neutered;
