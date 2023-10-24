bd63862d8cb69fe2c6397980b384268a24df706e
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-24 14:53:28 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -15,3 +15,13 @@ INSERT INTO public.animals(
 INSERT INTO public.animals(
 	name, date_of_birth, escape_attempts, neutered, weight_kg)
 	VALUES ('Devimon', '05.12.2017', '5', true, '11.00');
+
+INSERT INTO public.animals(
+	name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES ('Charmander', '2020-02-08', '0', false, '-11.00'),
+	('Plantmon', '2021-11-15', '2', true, '-5.7'),
+        ('Squirtle', '1993-04-02', '3', false, '-12.13'),
+	('Angemon', '2005-06-12', '1', true, '-45.00'),
+	('Boarmon', '2005-06-07', '7', true, '20.40'),
+	('Blossom', '1998-10-13', '3', true, '17.00'),
+	('Ditto', '2022-05-14', '4', true, '22.00');
