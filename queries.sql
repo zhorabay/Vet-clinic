42788bdf550e054a3c94adc2982716573b71d705
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-24 13:32:38 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
-SELECT name FROM animals WHERE date_of_birth BETWEEN 2016-01-01 AND 2019-12-31;
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE escape_attempts <3 and neutered = true;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.50;
