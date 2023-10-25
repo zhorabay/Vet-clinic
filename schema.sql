5ba92902ba83246680837aa798ea629cce44091b
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-25 12:46:51 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,3 +12,32 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  full_name character(50),
+  age integer,
+  CONSTRAINT owners_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name character(50),
+  CONSTRAINT owners_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+ADD PRIMARY KEY (id),
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT species_fkey
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT owners_fkey
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
