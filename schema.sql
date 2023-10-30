8eb489341a90c8c3cb65ebb409e08fad0e6d2b58
siddghosh108 <131663359+siddghosh108@users.noreply.github.com>
2023-10-30 03:57:03 -0700

    Fixed mistakes in schema.sql

--- schema.sql
+++ schema.sql
@@ -77,6 +77,6 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 ALTER TABLE owners ALTER COLUMN age DROP NOT NULL;
 
-CREATE INDEX idx_animal_id ON visits(animal_id);
-CREATE INDEX idx_vet_id ON visits(vet_id);
+CREATE INDEX idx_animals_id ON visits(animals_id);
+CREATE INDEX idx_vets_id ON visits(vets_id);
 CREATE INDEX idx_email ON owners(email);
